6cc76cdff68aeb35bef692e55f178015da221853
Batuhan <kucukyildizbatuhan@gmail.com>
2023-09-05 21:50:59 +0300

    Initial Commit

---
+++ SwiftFirebaseTest/SwiftFirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseTestApp.swift
+//  SwiftFirebaseTest
+//
+//  Created by Batuhan Küçükyıldız on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
